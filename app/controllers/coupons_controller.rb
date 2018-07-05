fecc068137e58685c5f6154c196e6892fc41a5db
Zac Willmington <zac.willmington@gmail.com>
2018-07-05 18:42:51 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -8,15 +8,15 @@ class CouponsController < ApplicationController
     end
 
     def create
+        binding.pry
         @coupon = Coupon.new
-        @coupon.coupon_code = params[:coupon_code]
-        @coupon.store = params[:store]
+        @coupon.coupon_code = params[:coupon][:coupon_code]
+        @coupon.store = params[:coupon][:store]
         @coupon.save
         redirect_to coupon_path(@coupon)
     end
 
     def show
-        binding.pry
         @coupon = Coupon.find(params[:id])
     end
 end
